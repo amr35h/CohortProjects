8b8346bc4f3871d7544ca45b6e55a32b2a2cc0f6
Amr35h <amreshchaurasiya21@gmail.com>
2025-03-31 13:57:20 +0530

    Backend Project Day 1 Setup Starts

---
+++ BackendMegaProject/src/utils/async-handler.js
@@ -0,0 +1,11 @@
+//Todo we may need later
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//Higher order function
+//Didn't required to use try catch everywhere
